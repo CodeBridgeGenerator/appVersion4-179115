7531b078a8c0819f5da235eb571379aab6bcfd74
CodeBridgeGenerator <kana@codebridge.my>
2025-09-02 14:35:55 +0800

    Code Bridge commit

---
+++ nodejs-backend/src/workersQue/index.js
@@ -0,0 +1,17 @@
+// const { createDynaLoaderQueWorker } = require('./processDynaLoaderQues');
+const {
+  createChangeForgotPasswordQueWorker,
+} = require("./processChangeForgotPasswordQue");
+const { createMailQueWorker } = require("./processEmails");
+const { createUserProfile } = require("./processCreateUserProfile");
+const { createUploader } = require("./processUploader");
+
+const createWorker = (app) => {
+  createJobQueWorker2(app);
+  createMailQueWorker(app);
+  createChangeForgotPasswordQueWorker(app);
+  createUserProfile(app);
+  createUploader(app);
+};
+
+module.exports = createWorker;
